2ce838b6f5ebde08f8659d919dfc26dd1c6d4e09
helino <erik.helin@gmail.com>
2013-10-15 15:34:04 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+CWD=$(pwd)
+ln -s $CWD/ackrc ~/.ackrc
+ln -s $CWD/elinks.conf ~/.elinks.conf
+ln -s $CWD/gitconfig ~/.gitconfig
+ln -s $CWD/gitignore_global ~/.gitignore_global
+ln -s $CWD/gvimrc ~/.gvimrc
+ln -s $CWD/hgrc ~/.hgrc
+ln -s $CWD/inputrc ~/.inputrc
+ln -s $CWD/muttrc ~/.muttrc
+ln -s $CWD/tmux.conf ~/.tmux.conf
+ln -s $CWD/vimperatorrc ~/.vimperatorrc
+ln -s $CWD/vimrc ~/.vimrc
+ln -s $CWD/zprofile ~/.zprofile
+ln -s $CWD/zshrc ~/.zshrc
