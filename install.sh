293990ecbe445bc18ac169279854ddeffa4ed071
Erik Helin <erik.helin@gmail.com>
2014-03-01 11:19:50 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -2,20 +2,7 @@
 
 set -e
 
-CWD=$(pwd)
-ln -s $CWD/ackrc ~/.ackrc
-ln -s $CWD/elinks.conf ~/.elinks.conf
-ln -s $CWD/gitconfig ~/.gitconfig
-ln -s $CWD/gitignore_global ~/.gitignore_global
-ln -s $CWD/gvimrc ~/.gvimrc
-ln -s $CWD/hgrc ~/.hgrc
-ln -s $CWD/inputrc ~/.inputrc
-ln -s $CWD/muttrc ~/.muttrc
-ln -s $CWD/msmtprc ~/.msmtprc
-ln -s $CWD/offlineimaprc ~/.offlineimaprc
-ln -s $CWD/offlineimap.py ~/.offlineimap.py
-ln -s $CWD/tmux.conf ~/.tmux.conf
-ln -s $CWD/vimperatorrc ~/.vimperatorrc
-ln -s $CWD/vimrc ~/.vimrc
-ln -s $CWD/zprofile ~/.zprofile
-ln -s $CWD/zshrc ~/.zshrc
+FILES=$(ls | grep -v 'README.md\|install.sh')
+for FILE in $FILES; do
+    ln -s $(pwd)/$FILE $HOME/.$FILE
+done
